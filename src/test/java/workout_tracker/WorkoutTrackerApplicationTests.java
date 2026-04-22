e7f99ee897dac74dbb89e592fdf9d87214d71272
Sai Varda <126225887+saivarda@users.noreply.github.com>
2026-04-22 10:28:09 -0500

    feat: add spring boot project scaffold

---
+++ src/test/java/workout_tracker/WorkoutTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package workout_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoutTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
